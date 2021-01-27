6f592e93afb890080b459d63c39e5547a35ab46d
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2021-01-27 12:23:25 +0100

    Template created

---
+++ Template/Template.cpp
@@ -0,0 +1,27 @@
+﻿// Template.cpp : Defines the entry point for the application.
+//
+
+#include "Template.h"
+
+int main()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    glm::vec3 test(10, 20, 30);
+    std::cout << test.x << " | " << test.y << " | " << test.z << std::endl;
+
+    return 0;
+}
